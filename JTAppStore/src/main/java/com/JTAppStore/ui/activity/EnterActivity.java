e7884c99118c2dc9ce1a6f6e5c8b391cd425ae61
JiangTao <422836331@qq.com>
2016-06-11 17:39:55 +0800

    2016061101

---
+++ JTAppStore/src/main/java/com/JTAppStore/ui/activity/EnterActivity.java
@@ -0,0 +1,13 @@
+package com.JTAppStore.ui.activity;
+
+import com.JTAppStore.ui.base.BaseAppCompatActivity;
+
+/**
+ * Created by JiangTao on 2016/6/11.
+ */
+public class EnterActivity extends BaseAppCompatActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
