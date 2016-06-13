b532ddbcce46603781dd67f47530588eb29a3af1
JiangTao <422836331@qq.com>
2016-06-13 22:28:05 +0800

    2016061302

--- JTHuaBan/src/main/java/jt/com/JTHuaBan/Module/Welcome/WelcomeActivity.java
+++ JTHuaBan/src/main/java/jt/com/JTHuaBan/Module/Welcome/WelcomeActivity.java
@@ -21,11 +21,9 @@ import rx.schedulers.Schedulers;
  * Created by JiangTao on 2016/6/11.
  */
 public class WelcomeActivity extends BaseActivity {
-    @Bind(R.id.img_welcome)
-    ImageView img_welcome;
-
-
-    //private ImageView img_welcome;
+//    @Bind(R.id.img_welcome)
+//    ImageView img_welcome;
+    private ImageView img_welcome;
     @Override
     protected String getTAG() {
         return WelcomeActivity.class.getSimpleName();
@@ -39,7 +37,7 @@ public class WelcomeActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //img_welcome=(ImageView)findViewById(R.id.img_welcome);
+        img_welcome=(ImageView)findViewById(R.id.img_welcome);
         //ButterKnife.bind(this);
     }
 
@@ -49,27 +47,27 @@ public class WelcomeActivity extends BaseActivity {
         //openActivity(MainActivity.class);
         Animator animator= AnimatorInflater.loadAnimator(mContext,R.animator.welcome_animator);
         animator.setTarget(img_welcome);
-        animator.start();
-//        WelcomeObservable.addAnimator(animator)
-//                .observeOn(Schedulers.io())
-//                .subscribeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<Void>() {
-//                    @Override
-//                    public void onCompleted() {
-//                        Log.d("Log","onCompleted");
-//                        openActivity(MainActivity.class);
-//                        finish();
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onNext(Void aVoid) {
-//
-//                    }
-//                });
+        //animator.start();
+        WelcomeObservable.addAnimator(animator)
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Void>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d("Log","onCompleted");
+                        openActivity(MainActivity.class);
+                        finish();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Void aVoid) {
+
+                    }
+                });
     }
 }
