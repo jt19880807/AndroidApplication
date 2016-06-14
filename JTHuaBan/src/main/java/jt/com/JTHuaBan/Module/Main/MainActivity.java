ed270ef4adca7e5535b7b92255e91f0e1f44d6b8
JiangTao <422836331@qq.com>
2016-06-14 22:30:09 +0800

    2016061402

--- JTHuaBan/src/main/java/jt/com/JTHuaBan/Module/Main/MainActivity.java
+++ JTHuaBan/src/main/java/jt/com/JTHuaBan/Module/Main/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends BaseActivity
     private String[] types;
     private String[] titles;
     private DrawerLayout drawer;
-    private  Toolbar toolbar;
+    private Toolbar toolbar;
     private FloatingActionButton fab;
     private NavigationView navigationView;
     @Override
@@ -109,19 +109,19 @@ public class MainActivity extends BaseActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
